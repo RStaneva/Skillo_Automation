65cc7c3391806ed7f0361e65469aba6397370b16
RalitsaStaneva <ralitsaivelinova@gmail.com>
2023-01-25 16:22:31 +0200

    Creating car objects with constructors

---
+++ src/lecture07/CarObjects.java
@@ -0,0 +1,11 @@
+package lecture07;
+
+public class CarObjects {
+    public static void main (String[] args){
+        Car car=new Car("Polo","silver",2007, 160, true);
+        Car car2=new Car("Audi","red",true);
+        Car car3=new Car("WV","yellow",2022, 300);
+        Car car4=new Car("BMW",2023, 350, true);
+        Car car5=new Car();
+    }
+}
